1077130a49c1f684ef60c822a26f38e86cf7b739
Jonathan Duncan <jonosuave@Jonathans-MacBook-Pro.local>
2017-01-28 12:41:10 -0700

    What I have prior to redoing the js a bit in order to accommodate negative numbers

--- Calculator.js
+++ Calculator.js
@@ -21,17 +21,36 @@
 //    }
 //}
 
+var n1 = 0;
+var n2 = undefined;
+var operator = undefined;
+
 function numberFunction(symbol){
-    var currentScreenText = document.getElementById('answer-block-number');
-    if(currentScreenText.innerHTML !== "0"){
-        currentScreenText.innerHTML = currentScreenText.innerHTML + symbol;
-    }else{
-        currentScreenText.innerHTML = symbol + "";
-    } 
+//    var currentScreenText = document.getElementById('answer-block-number');
+//    if(currentScreenText.innerHTML !== "0"){
+//        currentScreenText.innerHTML = currentScreenText.innerHTML + symbol;
+//    }else{
+//        currentScreenText.innerHTML = symbol + "";
+//    }
+    if(operator)
+    n2 = parseInt(symbol);
+    updateScreen();
 };
+
+function updateScreen() {
+    var currentScreenText = document.getElementById('answer-block-number');
+    if(!n2) {
+        currentScreenText.innerHTML = n1 +'';
+    } else if(n2 >=0) {
+        currentScreenText.innerHTML = n1 + ' ' + operator + ' ' + n2;
+    } else if (n2 < 0) {
+        currentScreenText.innerHTML = n1 + '(-' + n2 +')';
+    }
+}
 
 function decimalFunction(value) {
     currentScreenText.innerHTML = currentScreenText.innerHTML + value;
+    canDec = true;
 }
 
 
@@ -41,7 +60,9 @@ function clearScreen(){
 
 
 function combinator(){
-    var total = eval(document)
+    var screen = document.getElementById('answer-block-number');
+    var total = eval(screen.innerHTML);
+    screen.innerHTML = total + "";
 } 
 
 function calc(){
@@ -52,44 +73,54 @@ function calc(){
 
 
 $(document).ready(function() {
+    var canDec = true;
+    var canOperator = true;
     var screen = $('#answer-block-number');
-    var screenText = screen.html();
-    var symbol = $(this).attr('symbol');
     $('.operator').on('click',function(){
+        var symbol = $(this).attr('symbol');
         calc();
         $('.highlight').removeClass('highlight');
 //        screenText.html...hmmm, how do I clear out the operator sign from current screen.html()?
 //        The open html() returns current value
-        var symbol = $(this).attr('symbol');
         $(this).addClass('highlight');
-        screen.html(screenText + symbol);
-        
+        screen.html(screen.html() + symbol);
+        canDec = true;
+        canOperator=false;
     });
+    
+    $('.combinator').on('click',function() {
+        var symbol = $(this).attr('symbol');
+        combinator();
+        $('.highlight').removeClass('highlight');
+    })
     
     
     $('.number').on('click',function() {
         var symbol = $(this).attr('symbol');
         numberFunction(symbol);
+        canOperator = true;
     })
 //    $('#plus').on('click',additionFunction);
 //    $('#division').on('click',divisionFunction);
     
     $('#decimal').on('click',function(){
         $('.highlight').removeClass('highlight');
-        noMultipleDecimal();
-    });
-    function noMultipleDecimal() {
-    var decEnable = true;
-    if(decEnable){
-            screen.innerHTML = screenText + symbol;
-            decEnable = false;
-        }else {
-            alert("Yo this ain/'t English! Only one '.' mate." );
+        if(canDec){
+            screenText = screen.html();
+            screen.html(screenText + '.');
+            canDec = false;
         }
-    }
+    });
+    
+    $('.operator').on('click',function() {
+       if(canOperator) {
+           
+       } 
+    });
     
     $('#clear-button').on('click',function() {
-        $(this).closest('#Calculator-blue-background').find('.highlight').removeClass('highlight');
+        $('.highlight').removeClass('highlight');
+        canDec = true;
     });
     
 })
